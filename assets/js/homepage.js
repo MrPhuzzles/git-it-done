b2bda3377fb0196241a667b28301bf56652a78cc
MrPhuzzles <tieten.james92@gmail.com>
2022-08-20 15:13:39 -0400

    added filter buttons to search repos by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 // Form submission function
 var formSubmitHandler = function (event) {
@@ -68,6 +68,20 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
 // Function for getting repos of user
 var getUserRepos = function (user) {
     // format the github api url
@@ -93,5 +107,16 @@ var getUserRepos = function (user) {
 
 getUserRepos();
 
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    };
+};
+
 // Event listeners
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
